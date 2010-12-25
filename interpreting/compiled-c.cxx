e8c14684a1e12099f2b89b33b7c6ae03afeb4b87
scientific-coder <public-code@bernard-hugueney.org>
2010-12-25 18:55:47 +0100

    made a small compiled-c as a reference for interpreter efficiency  10x faster than assign-ops on my netbook

---
+++ interpreting/compiled-c.cxx
@@ -0,0 +1,19 @@
+#include <cstdlib>
+double f(double a, double b, double c){
+  volatile double d=555.666;
+  volatile double k;
+    for( std::size_t i(0); i != 1000; ++i) {
+      volatile double e=123.456;
+      volatile double f=128.256;
+      volatile double g=128.256;
+      volatile double h=2.5;
+      volatile double j=-1;
+      k=d+(e+f+(g-(h+j)));
+    }
+    return k;
+}
+int main(int argc, char* argv[]){
+  for(std::size_t i(0); i != 10000; ++i)
+    {  volatile double d=f(8888., -789., .75); } 
+  return 0;
+}
